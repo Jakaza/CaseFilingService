7500bd6681cfcfed4b8a3287269a54f7039d03d0
Themba G Chauke <69904835+Jakaza@users.noreply.github.com>
2024-10-31 14:57:22 +0200

    Create policeStationsData.js

---
+++ frontend/src/lib/policeStationsData.js
@@ -0,0 +1,304 @@
+export const policeStationsData = {
+    "Gauteng": {
+      "townships": {
+        "Soweto": [
+          "Soweto Police Station",
+          "Zola Police Station",
+          "Orlando Police Station",
+          "Diepkloof Police Station",
+          "Dube Police Station"
+        ],
+        "Tembisa": [
+          "Tembisa Police Station",
+          "Thembisa South Police Station",
+          "Tembisa East Police Station"
+        ],
+        "Alexandra": [
+          "Alexandra Police Station",
+          "Ramaphosa Police Station",
+          "Wattville Police Station"
+        ],
+        "Katlehong": [
+          "Katlehong Police Station",
+          "Thokoza Police Station"
+        ],
+        "Daveyton": [
+          "Daveyton Police Station",
+          "Etwatwa Police Station"
+        ],
+        "Soshanguve": [
+          "Soshanguve Police Station",
+          "Soshanguve North Police Station"
+        ],
+        "Pretoria": [
+          "Pretoria Central Police Station",
+          "Hatfield Police Station",
+          "Silverton Police Station"
+        ],
+        "Mamelodi": [
+          "Mamelodi Police Station",
+          "Mamelodi East Police Station"
+        ],
+        "Mabopane": [
+          "Mabopane Police Station"
+        ],
+        "Winterveld": [
+          "Winterveld Police Station"
+        ],
+        "Ga-Rankuwa": [
+          "Ga-Rankuwa Police Station"
+        ],
+        "Pretoria North": [
+          "Pretoria North Police Station"
+        ],
+        "Wonderboom": [
+          "Wonderboom Police Station"
+        ],
+        "Sinoville": [
+          "Sinoville Police Station"
+        ],
+        "Montana": [
+          "Montana Police Station"
+        ],
+        "Temba": [
+          "Temba Police Station"
+        ],
+        "Hammanskraal": [
+          "Hammanskraal Police Station"
+        ],
+        "Pretoria CBD": [
+          "Pretoria Central Police Station"
+        ],
+        "Brooklyn": [
+          "Brooklyn Police Station"
+        ],
+        "Hatfield": [
+          "Hatfield Police Station"
+        ],
+        "Pretoria West": [
+          "Pretoria West Police Station"
+        ],
+        "Centurion": [
+          "Centurion Police Station"
+        ],
+        "Irene": [
+          "Irene Police Station"
+        ],
+        "Rayton": [
+          "Rayton Police Station"
+        ],
+        "Roodeplaat": [
+          "Roodeplaat Police Station"
+        ],
+        "Cullinan": [
+          "Cullinan Police Station"
+        ],
+        "Pretoria East": [
+          "Pretoria East Police Station"
+        ],
+        "Eersterust": [
+          "Eersterust Police Station"
+        ],
+        "Shere": [
+          "Shere Police Station"
+        ],
+        "Bronkhorstspruit": [
+          "Bronkhorstspruit Police Station"
+        ]
+      }
+    },
+    "Western Cape": {
+      "townships": {
+        "Khayelitsha": [
+          "Khayelitsha Police Station",
+          "Mandela Police Station",
+          "Harare Police Station"
+        ],
+        "Mitchells Plain": [
+          "Mitchells Plain Police Station",
+          "Lentegeur Police Station",
+          "Portlands Police Station"
+        ],
+        "Nyanga": [
+          "Nyanga Police Station",
+          "Gugulethu Police Station",
+          "Philippi Police Station"
+        ],
+        "Steenberg": [
+          "Steenberg Police Station",
+          "Wynberg Police Station"
+        ]
+      }
+    },
+    "Eastern Cape": {
+      "townships": {
+        "Port Elizabeth": [
+          "Port Elizabeth Police Station",
+          "New Brighton Police Station",
+          "Uitenhage Police Station"
+        ],
+        "Gqeberha": [
+          "Gqeberha Police Station",
+          "Humewood Police Station",
+          "Kuyga Police Station"
+        ],
+        "Uitenhage": [
+          "Uitenhage Police Station",
+          "Despatch Police Station",
+          "Kamesh Police Station"
+        ],
+        "Mthatha": [
+          "Mthatha Police Station",
+          "Ngqeleni Police Station"
+        ],
+        "East London": [
+          "East London Police Station",
+          "King Williams Town Police Station"
+        ]
+      }
+    },
+    "KwaZulu-Natal": {
+      "townships": {
+        "Durban": [
+          "Durban Central Police Station",
+          "Hammarsdale Police Station",
+          "Chatsworth Police Station"
+        ],
+        "Pietermaritzburg": [
+          "Pietermaritzburg Police Station",
+          "Edendale Police Station",
+          "Plessislaer Police Station"
+        ],
+        "Newcastle": [
+          "Newcastle Police Station",
+          "Amajuba Police Station"
+        ],
+        "Empangeni": [
+          "Empangeni Police Station",
+          "Richards Bay Police Station"
+        ],
+        "Ulundi": [
+          "Ulundi Police Station",
+          "Ludheka Police Station"
+        ]
+      }
+    },
+    "Mpumalanga": {
+      "townships": {
+        "Mbombela": [
+          "Mbombela Police Station",
+          "Kanyamazane Police Station",
+          "Nelspruit Police Station"
+        ],
+        "Secunda": [
+          "Secunda Police Station",
+          "Evander Police Station",
+          "Trichardt Police Station"
+        ],
+        "Burgersfort": [
+          "Burgersfort Police Station",
+          "Steelpoort Police Station"
+        ],
+        "White River": [
+          "White River Police Station",
+          "Hazyview Police Station"
+        ],
+        "Giyani": [
+          "Giyani Police Station",
+          "Malamulele Police Station"
+        ]
+      }
+    },
+    "Limpopo": {
+      "townships": {
+        "Polokwane": [
+          "Polokwane Police Station",
+          "Mankweng Police Station",
+          "Bendor Police Station"
+        ],
+        "Thohoyandou": [
+          "Thohoyandou Police Station",
+          "Sibasa Police Station"
+        ],
+        "Tzaneen": [
+          "Tzaneen Police Station",
+          "Modjadjiskloof Police Station"
+        ],
+        "Mokopane": [
+          "Mokopane Police Station",
+          "Potgietersrus Police Station"
+        ],
+        "Malamulele": [
+          "Malamulele Police Station",
+          "Giyani Police Station"
+        ]
+      }
+    },
+    "Free State": {
+      "townships": {
+        "Bloemfontein": [
+          "Bloemfontein Police Station",
+          "Heidedal Police Station",
+          "Botshabelo Police Station"
+        ],
+        "Welkom": [
+          "Welkom Police Station",
+          "Thabong Police Station",
+          "Virginia Police Station"
+        ],
+        "Mangaung": [
+          "Mangaung Police Station",
+          "Phahameng Police Station"
+        ],
+        "Sasolburg": [
+          "Sasolburg Police Station",
+          "Vaalpark Police Station"
+        ]
+      }
+    },
+    "North West": {
+      "townships": {
+        "Mahikeng": [
+          "Mahikeng Police Station",
+          "Lichtenburg Police Station",
+          "Montshioa Police Station"
+        ],
+        "Rustenburg": [
+          "Rustenburg Police Station",
+          "Kroondal Police Station",
+          "Boitekong Police Station"
+        ],
+        "Klerksdorp": [
+          "Klerksdorp Police Station",
+          "Tshireletsang Police Station"
+        ],
+        "Brits": [
+          "Brits Police Station",
+          "Hartbeespoort Police Station"
+        ]
+      }
+    },
+    "Northern Cape": {
+      "townships": {
+        "Kimberley": [
+          "Kimberley Police Station",
+          "Galeshewe Police Station",
+          "Ritchie Police Station"
+        ],
+        "Upington": [
+          "Upington Police Station",
+          "Keimoes Police Station",
+          "Pofadder Police Station"
+        ],
+        "Postmasburg": [
+          "Postmasburg Police Station",
+          "Danielskuil Police Station"
+        ],
+        "De Aar": [
+          "De Aar Police Station",
+          "Boshof Police Station"
+        ]
+      }
+    }
+  }
+  
