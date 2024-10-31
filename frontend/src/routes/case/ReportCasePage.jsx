56fc93fb710e82a6bfef899d3be97b93750fc4dc
Themba G Chauke <69904835+Jakaza@users.noreply.github.com>
2024-10-31 14:18:13 +0200

    Update ReportCasePage.jsx

--- frontend/src/routes/case/ReportCasePage.jsx
+++ frontend/src/routes/case/ReportCasePage.jsx
@@ -31,13 +31,20 @@ const Modal = ({ isOpen, onClose, children }) => {
 
   return (
     <div className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full z-50">
-      <div className="relative top-20 mx-auto p-5 border w-96 shadow-lg rounded-md bg-white">
+      <div className="relative top-20 mx-auto p-5 border h-3/4 w-5/6 shadow-lg rounded-md bg-white">
         <div className="mt-3 text-center">
           {children}
-          <div className="items-center px-4 py-3">
+          <div className="items-center flex px-4 py-3">
             <button
               onClick={onClose}
-              className="px-4 py-2 bg-blue-500 text-white text-base font-medium rounded-md w-full shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-300"
+              className="px-4 py-2  mr-5 bg-blue-500 text-white text-base font-medium rounded-md w-auto shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-300"
+            >
+              Save & Submit
+            </button>
+
+            <button
+              onClick={onClose}
+              className="px-4 py-2 bg-blue-500 text-white text-base font-medium rounded-md w-auto shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-300"
             >
               Close
             </button>
