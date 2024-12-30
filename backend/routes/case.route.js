d7cc7112c16e9437e96d100f653dea849684ab3c
Themba G Chauke <GoodnessThembaC@gmail.com>
2024-12-30 09:40:23 +0200

    change url route

--- backend/routes/case.route.js
+++ backend/routes/case.route.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { close, open } from "../controllers/case.controller.js";
+import { close, open , view } from "../controllers/case.controller.js";
 const router = express.Router();
 
 // CITIZEN
+router.get("/view-case" , view);
 router.post("/open", open);
-router.post("/:caseId/close-request", close);
-router.get("/view");
+router.post("/close-request", close);
+
 
 export default router;
