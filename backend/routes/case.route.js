18553df45ebf6a725ee984deb8208b724f88e9e9
Themba G Chauke <GoodnessThembaC@gmail.com>
2025-01-04 00:25:31 +0200

    modified:   backend/routes/case.route.js

--- backend/routes/case.route.js
+++ backend/routes/case.route.js
@@ -1,11 +1,11 @@
 import express from "express";
-import { close, open , view } from "../controllers/case.controller.js";
+import { close, open, view, viewAll } from "../controllers/case.controller.js";
 const router = express.Router();
 
 // CITIZEN
-router.get("/view-case" , view);
+router.get("/view-case", view);
+router.get("/view-cases", viewAll);
 router.post("/open", open);
 router.post("/close-request", close);
-
 
 export default router;
